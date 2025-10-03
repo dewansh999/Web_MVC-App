1f220737d26120dedc88e52553ed73aec45cf58c
Dewansh Gupta <82531138+dewansh999@users.noreply.github.com>
2025-10-03 23:32:42 +0530

    Update UserController.java

--- src/main/java/in/expedia/controller/UserController.java
+++ src/main/java/in/expedia/controller/UserController.java
@@ -1,5 +1,7 @@
 package in.expedia.controller;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
