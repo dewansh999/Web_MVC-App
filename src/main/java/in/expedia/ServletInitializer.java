27af9286635cd987ad77015967ba8c6c3e432cec
Dewansh Gupta <82531138+dewansh999@users.noreply.github.com>
2025-10-30 23:58:19 +0530

    Update ServletInitializer.java

--- src/main/java/in/expedia/ServletInitializer.java
+++ src/main/java/in/expedia/ServletInitializer.java
@@ -1,5 +1,7 @@
 package in.expedia;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
